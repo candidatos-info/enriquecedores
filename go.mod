0c0d14c7519e1837237b315875ec6248dc35d0ba
abuarquemf <abuarquemf@gmail.com>
2020-06-15 23:22:08 -0300

    added basic auth

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/candidatos-info/enriquecedores
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import (
 	"os"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	userName := os.Getenv("USER_NAME")
+	if userName == "" {
+		log.Fatal("missing username on environment variables")
+	}
+	password := os.Getenv("PASSWORD")
+	if password == "" {
+		log.Fatal("missing password on environment variables")
+	}
 	e := echo.New()
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username != userName || password != password {
+			return false, nil
+		}
+		return true, nil
+	}))
 	e.POST("/cce", dispatchCCEHandler)
 	port := os.Getenv("SERVER_PORT")
 	if port == "" {
