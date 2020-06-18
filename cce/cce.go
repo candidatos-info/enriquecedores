3d4d1309b2b0a2bcf06e227c399ffdcf5e485505
abuarquemf <abuarquemf@gmail.com>
2020-06-18 19:33:54 -0300

    creting function

--- cce/cce.go
+++ cce/cce.go
@@ -1,6 +1,7 @@
 package cce
 
 import (
+	"archive/zip"
 	"bytes"
 	"crypto/md5"
 	"fmt"
@@ -9,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/candidatos-info/enriquecedores/status"
@@ -98,7 +100,55 @@ func executeForLocal(hash string, buf []byte) {
 }
 
 func unzipDownloadedFiles(bytes []byte) ([]os.FileInfo, error) {
-
+	unzipDesitination := "unziped"
+	r, err := zip.OpenReader(fileUnzip)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	os.MkdirAll(unzipDesitination, 0755)
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		path := filepath.Join(unzipDesitination, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func executeForGCP() {
