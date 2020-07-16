ab224a60d20b5d469fdd314e6c3cfd07b38e135d
abuarquemf <abuarquemf@gmail.com>
2020-07-16 14:44:59 -0300

    added comment

--- set_env_vars.py
+++ set_env_vars.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     file_content = ""
     with open (app_engine_file, "r") as file:
         app_engine_file_content = file.read()
-        line = re.sub(r"##PASSWORD", password, app_engine_file_content) 
-        line = re.sub(r"##USER_NAME", user_name, line)
+        line = re.sub(r"##PASSWORD", password, app_engine_file_content) # replacing ##PASSWORD by the given password
+        line = re.sub(r"##USER_NAME", user_name, line) # replacing ##USER_NAME by the given user name
         file_content = line
     with open (app_engine_file, "w") as file:
         file.write(file_content)
