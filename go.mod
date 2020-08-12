862f94e449fcb599e6fca600173949d7304a4afd
abuarquemf <abuarquemf@gmail.com>
2020-08-12 14:05:13 -0300

    fixing on descritor changes

--- go.mod
+++ go.mod
@@ -3,16 +3,25 @@ module github.com/candidatos-info/enriquecedores
 go 1.13
 
 require (
-	cloud.google.com/go/datastore v1.1.0
+	cloud.google.com/go v0.63.0 // indirect
+	cloud.google.com/go/bigquery v1.10.0 // indirect
+	cloud.google.com/go/datastore v1.2.0
+	cloud.google.com/go/pubsub v1.6.1 // indirect
 	cloud.google.com/go/storage v1.10.0
-	github.com/candidatos-info/descritor v0.0.0-20200715141214-1d311ce610eb
+	github.com/candidatos-info/descritor v0.0.0-20200812162116-697cf5b508a2
 	github.com/cheekybits/is v0.0.0-20150225183255-68e9c0620927 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gocarina/gocsv v0.0.0-20200330101823-46266ca37bd3
+	github.com/golang/protobuf v1.4.2
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/matryer/is v1.4.0 // indirect
 	github.com/matryer/try v0.0.0-20161228173917-9ac251b645a2
 	github.com/valyala/fasttemplate v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20200812155832-6a926be9bd1d // indirect
 	golang.org/x/text v0.3.3
+	golang.org/x/tools v0.0.0-20200811215021-48a8ffc5b207 // indirect
+	google.golang.org/genproto v0.0.0-20200812160120-2e714abc8b50 // indirect
 	gopkg.in/matryer/try.v1 v1.0.0-20150601225556-312d2599e12e
+	honnef.co/go/tools v0.0.1-2020.1.5 // indirect
 )
--- tse_db_index/main.go
+++ tse_db_index/main.go
@@ -15,30 +15,16 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"cloud.google.com/go/datastore"
 	"github.com/candidatos-info/descritor"
-	"github.com/candidatos-info/enriquecedores/tse_utils"
+	tseutils "github.com/candidatos-info/enriquecedores/tse_utils"
 	"github.com/gocarina/gocsv"
 	"golang.org/x/text/encoding/charmap"
 )
 
 const (
 	candidaturesCollection = "candidatures"
-)
-
-var (
-	rolesMap = map[string]descritor.Cargo{
-		"VEREADOR":      "LM",  // Legislativo Municipal
-		"VICE-PREFEITO": "VEM", // Vice Executivo Municipal
-		"PREFEITO":      "EM",  // Executivo Municipal
-	}
-
-	declaredPossessions = map[string]bool{
-		"S": true,
-		"N": false,
-	}
 )
 
 // TODO refact these items
@@ -231,11 +217,11 @@ func process(outDir, state string, client *datastore.Client) error {
 		r.Comma = ';'
 		return r
 	})
-	var c []*Candidatura
+	var c []*tseutils.RegistroTSE
 	if err := gocsv.UnmarshalFile(file, &c); err != nil {
 		return fmt.Errorf("falha ao inflar slice de candidaturas usando arquivo csv %s, erro %q", file.Name(), err)
 	}
-	filteredCandidatures, err := removeDuplicates(c, path.Base(pathToOpen))
+	filteredCandidatures, err := tseutils.RemoveDuplicates(c, path.Base(pathToOpen))
 	if err != nil {
 		return fmt.Errorf("falha ao remover candidaturas duplicadas, erro %q", err)
 	}
@@ -267,80 +253,4 @@ func findCandidateByLegalCode(legalCode string, client *datastore.Client) (*tseu
 		return nil, fmt.Errorf("falha ao buscar candidato por CPF, erro %q", err)
 	}
 	return c[0], nil
-}
-
-// it iterates through the candidates list and returns a map of
-// struct *descritor.Candidatura where the key is the candidate CPF.
-// To handle the duplicated canidate data lines is used the candidate
-// CPF as search key.
-// This is necessary due to the fact that TSE CSV duplicate candidate's
-// data if it goes to the election second round, changing only two columns:
-// the election round (NR_TURNO) and the candidature situation (DS_SIT_TOT_TURNO).
-// This function takes care of it by collecting the candidate only once and
-// registering if it has gone or not to election second round.
-func removeDuplicates(candidates []*Candidatura, fileBeingHandled string) (map[string]*descritor.Candidatura, error) {
-	candidatesMap := make(map[string]*descritor.Candidatura)
-	fileLines := 0
-	duplicatedLines := 0
-	for _, c := range candidates {
-		foundCandidate := candidatesMap[c.CPF]
-		if foundCandidate == nil { // candidate not present on map, add it
-			fileLines++
-			var candidateBirth time.Time
-			if c.Candidato.Nascimento != "" {
-				candidateBirth, err := time.Parse("02/01/2006", c.Candidato.Nascimento)
-				if err != nil {
-					return nil, fmt.Errorf("falha ao fazer parse da data de nascimento do candidato [%s] para o formato 02/01/2006, erro %q", c.Candidato.Nascimento, err)
-				}
-				_ = candidateBirth
-			}
-			newCandidate := &descritor.Candidatura{
-				Legislatura:         c.Legislatura,
-				Cargo:               rolesMap[c.Cargo],
-				UF:                  c.UF,
-				Municipio:           c.Municipio,
-				NomeUrna:            c.NomeUrna,
-				Aptidao:             c.Aptidao,
-				Deferimento:         c.Deferimento,
-				TipoAgremiacao:      c.TipoAgremiacao,
-				NumeroPartido:       c.NumeroPartido,
-				NumeroUrna:          c.NumeroUrna,
-				LegendaPartido:      c.LegendaPartido,
-				NomePartido:         c.NomePartido,
-				NomeColigacao:       c.NomeColigacao,
-				PartidosColigacao:   c.PartidosColigacao,
-				DeclarouBens:        declaredPossessions[c.DeclarouBens],
-				SequencialCandidato: c.SequencialCandidato,
-				Candidato: descritor.Candidato{
-					UF:              c.Candidato.UF,
-					Municipio:       c.Candidato.Municipio,
-					Nascimento:      candidateBirth,
-					TituloEleitoral: c.Candidato.TituloEleitoral,
-					Genero:          c.Candidato.Genero,
-					GrauInstrucao:   c.Candidato.GrauInstrucao,
-					EstadoCivil:     c.Candidato.EstadoCivil,
-					Raca:            c.Candidato.Raca,
-					Ocupacao:        c.Candidato.Ocupacao,
-					CPF:             c.Candidato.CPF,
-					Nome:            c.Candidato.Nome,
-					Email:           c.Candidato.Email,
-				},
-			}
-			if c.Turno == 1 {
-				newCandidate.SituacaoPrimeiroTurno = c.Situacao
-			} else {
-				newCandidate.SituacaoSegundoTurno = c.Situacao
-			}
-			candidatesMap[c.CPF] = newCandidate
-		} else { // candidate already on map (maybe election second round)
-			duplicatedLines++
-			if c.Turno == 1 {
-				foundCandidate.SituacaoPrimeiroTurno = c.Situacao
-			} else {
-				foundCandidate.SituacaoSegundoTurno = c.Situacao
-			}
-		}
-	}
-	log.Printf("file [%s], lines [%d], duplicated lines [%d]\n", fileBeingHandled, fileLines, duplicatedLines)
-	return candidatesMap, nil
 }
