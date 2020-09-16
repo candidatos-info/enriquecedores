15d776795f5b15049f95e9ec6dbdf9bb3cef94ec
abuarquemf <abuarquemf@gmail.com>
2020-09-16 13:12:41 -0300

    loggin processed pictures to a file

--- ballot_picture/pictures_test.go
+++ ballot_picture/pictures_test.go
@@ -28,18 +28,27 @@ func TestProcessWithPictureHavingCorrespondentCandidate(t *testing.T) {
 	if err := ioutil.WriteFile(fakeCandidatureFileName, []byte(dumbMessage), 0644); err != nil {
 		t.Errorf("expected error nil when create file [%s] to simulate a candidature file, got %q", fakeCandidatureFileName, err)
 	}
-	if err := process(picturesDir, storageDir, filestorage.NewLocalStorage()); err != nil {
+	logFileName := "log_file.txt"
+	logFile, err := os.Create(logFileName)
+	if err != nil {
+		t.Errorf("expected err nil when creating log file, go %v", err)
+	}
+	defer logFile.Close()
+	if err := process(picturesDir, storageDir, filestorage.NewLocalStorage(), logFile); err != nil {
 		t.Errorf("expected error nil when running process, error %q", err)
 	}
 	expectedFileToFindOnStorageDirAfterProcess := fmt.Sprintf("%s/%s_%s.jpg", storageDir, picturesDir, fakeSequencialCandidate)
-	_, err := os.Stat(expectedFileToFindOnStorageDirAfterProcess)
+	_, err = os.Stat(expectedFileToFindOnStorageDirAfterProcess)
 	if err != nil {
 		t.Errorf("expected error nil when running stat on expected file, got %q", err)
 	}
 	if err := os.RemoveAll(storageDir); err != nil {
-		t.Errorf("expected erro nil when removing created files")
+		t.Errorf("expected erro nil when removing created files, erro %v", err)
 	}
 	if err := os.RemoveAll(picturesDir); err != nil {
-		t.Errorf("expected erro nil when removing created files")
+		t.Errorf("expected erro nil when removing created files, erro %v", err)
+	}
+	if err := os.RemoveAll(logFileName); err != nil {
+		t.Errorf("expected erro nil when removing log file, erro %v", err)
 	}
 }
